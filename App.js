a5226748e436f97c605f859f5a2e12333f0cc95a
Priban <lukaspriban01@gmail.com>
2023-05-04 12:51:57 +0200

    socket

--- App.js
+++ App.js
@@ -1,31 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, StyleSheet, TextInput, View, Text, FlatList, TouchableOpacity } from 'react-native';
 import PaperView from './Components/PaperView';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Message from './Components/Message';
 import Register from './Components/Register';
 import axios from 'axios';
+import io from 'socket.io-client';
 
 export default function App() {
 
   const [text, setText] = useState('');
+  const [socket, setSocket] = useState(null);
   const [messages, setMessages] = useState([
     { username: "Já", text: "Olá" },
     { username: "Já", text: "Haha" },
   ]);
+
+  useEffect(() => {
+
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    }
+  }, []);
 
   const onPress = () => {
     setMessages([...messages, { username: "Test", text: text }]);
     setText('');
   }
 
+  // 1. dokumentace socket.io
+  // 2. vytvořit instanci socket.io a nastavit token z
+  //    response.data.token
+  // 3. instance socket.io musí být uložena ve state
+  // 4. nastavit listener na event "message",
+  //    zpráva je ve formátu { username: "Já", text: "Olá" }
+  // 5. přidat zprávu do messages
+  // 6. enjoy
   const register = async () => {
-    const response = await axios.post('https://lprib.tech/register', {
-      username: "test",
-      password: "test123"
+    const { data: { token } } = await axios.post('https://lprib.tech/register', {
+      username: "testinguser",
+      password: "test123sdf"
     });
 
-    console.log(response);
+    const socket = io('https://lprib.tech', {
+      query: {
+        token
+      },
+      transports: ['websocket']
+    });
+
+    socket.on('message', message => {
+      setMessages([...messages, message]);
+    });
+
+    setSocket(socket);
   }
 
   return (
@@ -67,10 +97,10 @@ const styles = StyleSheet.create({
   chatInput: {
     width: "90vw"
   },
-  messageTextContainer: { 
-    flex: 1, 
+  messageTextContainer: {
+    flex: 1,
     width: "100vw",
     justifyContent: "flex-end",
-    alignItems: "flex-start" 
+    alignItems: "flex-start"
   }
 });
